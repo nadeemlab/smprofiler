e4807e4ba9a3c8fad6767646c48da5af39a6dbe4
James Mathews <mail@jmath.read-books.org>
2025-11-26 01:19:43 -0500

    Restore cli entrypoint into report generation, and test

--- test/apiserver/module_tests/test_pdf_generation.sh
+++ test/apiserver/module_tests/test_pdf_generation.sh
@@ -2,5 +2,6 @@
 SMPROFILER_FAST_POLLING=1 smprofiler workflow generate-analysis-report \
  "Melanoma intralesional IL2" \
  --database-config-file="../apiserver/.smprofiler_db.config.container" \
- --api-server="http://smprofiler-apiserver-testing-apiserver"
+ --api-server="http://smprofiler-apiserver-testing-apiserver" \
+ --omitted-cohorts=2,4
 
