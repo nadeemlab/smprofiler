b4dc7028874c2710cc60ab25216d802a5e7ea16e
James Mathews <mail@jmath.read-books.org>
2026-01-06 17:06:22 -0500

    Start making formalized script entrypoint

--- smprofiler/workflow/scripts/automated_analysis.py
+++ smprofiler/workflow/scripts/automated_analysis.py
@@ -4,7 +4,7 @@ from os import chdir
 from os import mkdir
 from os.path import exists
 from os import environ as os_environ
-import sys
+from argparse import ArgumentParser
 
 from smprofiler.workflow.automated_analysis.types import Result
 from smprofiler.workflow.automated_analysis.pdf_generator import PDFReportGenerator
@@ -16,13 +16,27 @@ def result_quality(r: Result) -> float:
     return -1 * r.quality()
 
 if __name__=='__main__':
-    dbc = sys.argv[1]
-    api_host = sys.argv[2]
+    parser = ArgumentParser(
+        prog='smprofiler workflow automated-analysis',
+        description='Perform basic automated multi-feature comparison analysis on one or several datasets.'
+    )
+    parser.add_argument('--database-config-file', required=True)
+    parser.add_argument('--api-hostname', required=True)
+    parser.add_argument('--analysis-options-json', required=True, help='''
+List of items with study "handle", and optionally "omitted_cohorts",
+"omitted_channels", and "omit_proximity" (i.e. skip doing proximity
+metric comparisons, for performance).''')
+    args = parser.parse_args()
+
+    dbc = args.database_config_file
+    api_host = args.api_hostname
+    options_file = args.analysis_options_json
+
     credentials = retrieve_credentials_from_file(dbc)
     os_environ['SINGLE_CELL_DATABASE_HOST'] = credentials.endpoint
     os_environ['SINGLE_CELL_DATABASE_USER'] = credentials.user
     os_environ['SINGLE_CELL_DATABASE_PASSWORD'] = credentials.password
-    studies = json.loads(open('study_names.json', 'rt', encoding='utf-8').read())
+    studies = json.loads(open(options_file, 'rt', encoding='utf-8').read())
     for entry in studies:
         study = entry['handle']
         print('Doing ' + study + '.')
@@ -36,7 +50,8 @@ if __name__=='__main__':
         chdir(subdir)
         oc = 'omitted_channels'
         omitted_channels = entry[oc] if oc in entry else None
-        generator = PDFReportGenerator(*context, omitted_cohorts=omitted_cohorts, omitted_channels=omitted_channels)
+        omit_proximity = entry['omit_proximity'] if 'omit_proximity' in entry else False
+        generator = PDFReportGenerator(*context, omitted_cohorts=omitted_cohorts, omitted_channels=omitted_channels, omit_proximity=omit_proximity)
         generator.generate_and_save()
         server = PDFReportServer(*context[1:])
         pdf = server.datestamp_and_retrieve()
