ff9da84092d4cb2013feec40b42b671f8f05109f
James Mathews <mail@jmath.read-books.org>
2026-01-23 14:19:18 -0500

    Fix mismatch between pedantic and non-pedantic diagnosis record types, reuse prior uploader

--- smprofiler/workflow/tabular_import/parsing/permanent_event.py
+++ smprofiler/workflow/tabular_import/parsing/permanent_event.py
@@ -2,11 +2,14 @@
 import re
 from os.path import join
 import importlib.resources
+from importlib.resources import as_file
+from importlib.resources import files
 
 from pandas import read_csv
 from pandas import DataFrame
 from psycopg import Connection as PsycopgConnection
 
+from smprofiler.workflow.tabular_import.parsing.diagnosis import DiagnosisParser
 from smprofiler.standalone_utilities.log_formats import colorized_logger
 logger = colorized_logger(__name__)
 
@@ -30,7 +33,7 @@ class TableTranscriber:
     def _transcribe_table(self, table_file: str) -> None:
         df = read_csv(table_file, sep='\t')
         logger.info(f'Considering {table_file}: {df}')
-        table_name = re.sub(r'\.tsv$', '', table_file)
+        table_name = re.search(r'([^/]+)\.tsv$', table_file).group(1)
         df.columns = list(map(_normalize_column, list(df.columns)))
         self._validate_schema(df, table_name)
         columns = _get_columns(table_name)
@@ -48,19 +51,39 @@ class TableTranscriber:
     def _validate_schema(self, df: DataFrame, table_name: str) -> None:
         columns = _get_columns(table_name)
         if columns != tuple(df.columns):
-            raise ValueError(f'Schema for supplied table (columns: {df.columns}) does not match: {columns}')
+            raise ValueError(f'Schema for supplied table (columns: {df.columns}) does not match "{table_name}": {columns}')
 
-def get_permanent_events_transcriber(file_path: str, connection: PsycopgConnection) -> TableTranscriber:
-    tables = ('permanent_condition_diagnosis.tsv', 'condition_lack.tsv', 'diagnosis.tsv')
-    table_files = tuple(map(lambda t: join(file_path, t), tables))
-    return TableTranscriber(table_files, connection)
+
+class SurvivalDataTranscriber:
+    file_path: str
+    connection: PsycopgConnection
+    t: TableTranscriber
+
+    def __init__(self, file_path: str, connection: PsycopgConnection):
+        tables = ('permanent_condition_diagnosis.tsv', 'condition_lack.tsv')
+        table_files = tuple(map(lambda t: join(file_path, t), tables))
+        self.file_path = file_path
+        self.connection = connection
+        self.t = TableTranscriber(table_files, connection)
+
+    def transcribe(self) -> None:
+        self.t.transcribe()
+        diagnosis_file = join(self.file_path, 'diagnosis.tsv')
+        DiagnosisParser(get_fields()).parse(self.connection, diagnosis_file, drop_first=True)
+
+
+def get_permanent_events_transcriber(file_path: str, connection: PsycopgConnection) -> SurvivalDataTranscriber:
+    return SurvivalDataTranscriber(file_path, connection)
 
 def _normalize_column(c: str) -> str:
     return re.sub(' ', '_', c).lower()
 
 def _get_columns(table: str) -> tuple[str, ...]:
-    with importlib.resources.path('adiscstudies', 'fields.tsv') as path:
-        fields = read_csv(path, sep='\t')
+    fields = get_fields()
     return tuple(map(_normalize_column, list(fields[fields['Table'].apply(_normalize_column) == table]['Label'])))
 
-
+def get_fields():
+    with importlib.resources.path('adiscstudies', 'fields.tsv') as path:
+        fields = read_csv(path, sep='\t')
+    return fields
+ 
